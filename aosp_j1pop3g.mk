98c7eff48c8b81f316b2b62a09bff80f332b68d0
horoid <59495159+horoidrom@users.noreply.github.com>
2022-07-30 14:09:24 +0530

    Update and rename cm.mk to aosp_j1pop3g.mk

--- aosp_j1pop3g.mk
+++ aosp_j1pop3g.mk
@@ -1,18 +1,18 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+#$(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := j1pop3g
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+#$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/j1pop3g/device_core33g.mk)
+$(call inherit-product, device/samsung/j1pop3g/device_j1pop3g.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := j1pop3g
-PRODUCT_NAME := cm_j1pop3g
+PRODUCT_NAME := aosp_j1pop3g
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-J110H
 PRODUCT_MANUFACTURER := samsung
